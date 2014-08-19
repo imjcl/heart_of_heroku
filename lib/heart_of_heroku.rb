3c8e030442dfd6a1d051f1266776428ba932f8e2
James Liu <jaycliu@gmail.com>
2014-08-19 15:11:20 -0700

    add require for rest-client, account files, and constant BASE_URI

--- lib/heart_of_heroku.rb
+++ lib/heart_of_heroku.rb
@@ -1,6 +1,8 @@
-require 'heart_of_heroku/version'
+require "rest-client"
+require "heart_of_heroku/account"
 
 module HeartOfHeroku
+  BASE_URI = "https://api.heroku.com"
   def self.hi
     puts "Hello world!"
   end
