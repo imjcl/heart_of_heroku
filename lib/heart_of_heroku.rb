445d08e573968443f7c76fedbec78d9ed670baa2
James Liu <jaycliu@gmail.com>
2014-08-19 15:14:11 -0700

    add ACCEPT_HEADER constant, since all requests must use it

--- lib/heart_of_heroku.rb
+++ lib/heart_of_heroku.rb
@@ -3,6 +3,7 @@ require "heart_of_heroku/account"
 
 module HeartOfHeroku
   BASE_URI = "https://api.heroku.com"
+  ACCEPT_HEADER = "application/vnd.heroku+json; version=3"
   def self.hi
     puts "Hello world!"
   end
