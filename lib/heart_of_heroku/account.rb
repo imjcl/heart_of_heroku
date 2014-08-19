612bce4fa753204e5a2941b931890774492661fe
James Liu <jaycliu@gmail.com>
2014-08-19 15:25:55 -0700

    create method Account.get_info to return informaiton from Accout endpoint

--- lib/heart_of_heroku/account.rb
+++ lib/heart_of_heroku/account.rb
@@ -1,4 +1,7 @@
 module HeartOfHeroku
   class Account
+    def self.get_info token
+      RestClient.get "#{BASE_URI}/account", accept: "#{ACCEPT_HEADER}", authorization: "Bearer #{token}"
+    end
   end
 end
