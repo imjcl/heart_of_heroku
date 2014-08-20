81cfbc71c690ee8b73358f93a65a22b1ca54cbc0
James Liu <jaycliu@gmail.com>
2014-08-19 17:38:40 -0700

    refactor Apps, to use heroku_get

--- lib/heart_of_heroku/apps.rb
+++ lib/heart_of_heroku/apps.rb
@@ -1,7 +1,7 @@
 module HeartOfHeroku
   class Apps
     def self.get_apps
-      RestClient.get "#{BASE_URI}/apps", accept: "#{ACCEPT_HEADER}"
+      HeartOfHeroku::heroku_get "apps"
     end
   end
 end
