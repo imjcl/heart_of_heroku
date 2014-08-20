b48e50b74b0aef3d6ce3f4dd91400e3b43a74c2e
James Liu <jaycliu@gmail.com>
2014-08-19 17:13:35 -0700

    create heroku_get, this method sends a get request to an endpoint, with proper headers

--- lib/heart_of_heroku.rb
+++ lib/heart_of_heroku.rb
@@ -13,4 +13,8 @@ module HeartOfHeroku
   def self.set_auth_key token
     AUTH_KEY = token
   end
+
+  def self.heroku_get endpoint
+    JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADER, authorization: "Bearer #{AUTH_KEY}"
+  end
 end
