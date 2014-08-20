1ac716eac3b70cb11da326dcb9429a70e12d6ab3
James Liu <jaycliu@gmail.com>
2014-08-19 17:58:17 -0700

    add method, get_app to Apps, for individual application information

--- lib/heart_of_heroku/apps.rb
+++ lib/heart_of_heroku/apps.rb
@@ -3,5 +3,9 @@ module HeartOfHeroku
     def self.get_apps
       HeartOfHeroku::heroku_get "apps"
     end
+
+    def self.get_app id_name
+      HeartOfHeroku::heroku_get "apps/#{id_name}"
+    end
   end
 end
