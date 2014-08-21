e9743e7fed7b1a200ee394fb8ed0f50491f7065a
James Liu <jaycliu@gmail.com>
2014-08-20 19:42:43 -0700

    change Account.get_info to take auth_key as a parameter, since it is now tied to a user

--- lib/heart_of_heroku/account.rb
+++ lib/heart_of_heroku/account.rb
@@ -1,7 +1,7 @@
 module HeartOfHeroku
   class Account
-    def self.get_info
-     HeartOfHeroku::heroku_get "account"
+    def self.get_info auth_key
+     HeartOfHeroku::heroku_get "account", auth_key
     end
   end
 end
