da4b499a9606aed1da2beac248fc97802f2fab3c
James Liu <jaycliu@gmail.com>
2014-08-20 19:43:20 -0700

    refactor apps.rb to also accept auth_key in both of its methods

--- lib/heart_of_heroku/apps.rb
+++ lib/heart_of_heroku/apps.rb
@@ -1,11 +1,11 @@
 module HeartOfHeroku
   class Apps
-    def self.get_apps
-      HeartOfHeroku::heroku_get "apps"
+    def self.get_apps auth_key
+      HeartOfHeroku::heroku_get "apps", auth_key
     end
 
-    def self.get_app id_name
-      HeartOfHeroku::heroku_get "apps/#{id_name}"
+    def self.get_app id_name, auth_key
+      HeartOfHeroku::heroku_get "apps/#{id_name}", auth_key
     end
   end
 end
