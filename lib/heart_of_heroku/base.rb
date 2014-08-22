2c1d5424137614c6c11463f82ff03f64628961b1
James Liu <jaycliu@gmail.com>
2014-08-21 20:24:33 -0700

    create initializer for base

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -1,8 +1,10 @@
 module HeartOfHeroku
   class Base
-    @base_uri = "https://api.heroku.com/"
-    @accept_headers = "application/vnd.heroku+json; version=3"
-    @endpoint = ""
+    def initialize
+      @base_uri = "https://api.heroku.com/"
+      @accept_headers = "application/vnd.heroku+json; version=3"
+    end
+    
     def find options={}
       JSON.parse RestClient.get "#{@base_uri}/#{@endpoint}", accept: @accept_headers, authorization: "Bearer #{@auth_key}"
     end
