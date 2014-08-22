09466f41ceaa221580961b22f54823fb3e0e03d1
James Liu <jaycliu@gmail.com>
2014-08-21 21:38:09 -0700

    create debug to check path

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -6,6 +6,7 @@ module HeartOfHeroku
     def find endpoint, option=''
       path = "#{BASE_URI}/#{endpoint}"
       path += "/#{option}" unless option.empty?
+      puts path
       JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
     end
   end
