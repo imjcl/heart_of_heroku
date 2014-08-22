102ba58363a33b39c75d1be14d2c3861eb86d692
James Liu <jaycliu@gmail.com>
2014-08-21 20:38:56 -0700

    fixed typo for BASE_URL to BASE_URI

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -1,10 +1,10 @@
 module HeartOfHeroku
   class Base
-    BASE_URI = "https://api.heroku.com/"
+    BASE_URI = "https://api.heroku.com"
     ACCEPT_HEADERS = "application/vnd.heroku+json; version=3"
 
     def find options={}
-      JSON.parse RestClient.get "#{BASE_URL}/#{@endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
+      JSON.parse RestClient.get "#{BASE_URI}/#{@endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
     end
   end
 end
