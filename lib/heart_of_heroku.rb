ac28f991575799355baeeb1cc601e52b14cca898
James Liu <jaycliu@gmail.com>
2014-08-21 17:19:11 -0700

    refactor heart to have a base class that other classes will inherit

--- lib/heart_of_heroku.rb
+++ lib/heart_of_heroku.rb
@@ -6,13 +6,12 @@ require "heart_of_heroku/apps"
 require "heart_of_heroku/client"
 
 module HeartOfHeroku
-  BASE_URI = "https://api.heroku.com"
-  ACCEPT_HEADER = "application/vnd.heroku+json; version=3"
-  def self.hi
-    puts "Hello world!"
-  end
+  class Base
+    BASE_URI = "https://api.heroku.com"
+    ACCEPT_HEADER = "application/vnd.heroku+json; version=3"
 
-  def self.heroku_get endpoint, auth_key
-    JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADER, authorization: "Bearer #{auth_key}"
+    def find options = {}
+      JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADER, authorization: "Bearer #{@auth_key}"  
+    end
   end
 end
