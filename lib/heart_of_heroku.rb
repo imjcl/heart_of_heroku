23c56f0d8b0ad474705ff0dcd0333034e92b63a9
James Liu <jaycliu@gmail.com>
2014-08-21 17:22:23 -0700

    refactor changing constants to class variables, that will be adjusted in subclasses..hopefully.

--- lib/heart_of_heroku.rb
+++ lib/heart_of_heroku.rb
@@ -1,17 +1,16 @@
 require "rest-client"
 require "json"
 
-require "heart_of_heroku/account"
-require "heart_of_heroku/apps"
 require "heart_of_heroku/client"
 
 module HeartOfHeroku
   class Base
-    BASE_URI = "https://api.heroku.com"
-    ACCEPT_HEADER = "application/vnd.heroku+json; version=3"
+    self.base_uri = "https://api.heroku.com"
+    self.accept_header = "application/vnd.heroku+json; version=3"
+    self.endpoint = ''
 
     def find options = {}
-      JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADER, authorization: "Bearer #{@auth_key}"  
+      JSON.parse RestClient.get "#{base_uri}/#{endpoint}", accept: accept_header, authorization: "Bearer #{@auth_key}"  
     end
   end
 end
