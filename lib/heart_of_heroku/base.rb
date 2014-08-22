88c76b6ad06df18c00e4ae8899fb34d616a442e5
James Liu <jaycliu@gmail.com>
2014-08-21 21:13:31 -0700

    changed how find takes arguments

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -3,8 +3,8 @@ module HeartOfHeroku
     BASE_URI = "https://api.heroku.com"
     ACCEPT_HEADERS = "application/vnd.heroku+json; version=3"
 
-    def find options={}
-      JSON.parse RestClient.get "#{BASE_URI}/#{@endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
+    def find endpoint, options={}
+      JSON.parse RestClient.get "#{BASE_URI}/#{endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
     end
   end
 end
