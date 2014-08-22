482d075fa0ee5d0f4aa0f64c5ddd61fe4cd9ee3e
James Liu <jaycliu@gmail.com>
2014-08-21 17:22:54 -0700

    redefine Client class

--- lib/heart_of_heroku/client.rb
+++ lib/heart_of_heroku/client.rb
@@ -1,19 +1,4 @@
 module HeartOfHeroku
-  class Client
-    def initialize token
-      @auth_key = token
-    end
-
-    def account
-      @account ||= Account.get_info(@auth_key)
-    end
-
-    def list_apps
-      @apps ||= Apps.get_apps(@auth_key)
-    end
-
-    def list_app_by_id id
-      Apps.get_app(id, @auth_key)
-    end
+  class Client < Base
   end
 end
