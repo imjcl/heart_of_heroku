4019203c6f8a73ff3f01b9a6b032cda3545ba8e7
James Liu <jaycliu@gmail.com>
2014-08-21 20:31:48 -0700

    change some of our true constants back to constants

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -1,12 +1,10 @@
 module HeartOfHeroku
   class Base
-    def initialize
-      @base_uri = "https://api.heroku.com/"
-      @accept_headers = "application/vnd.heroku+json; version=3"
-    end
-    
+    BASE_URI = "https://api.heroku.com/"
+    ACCEPT_HEADERS = "application/vnd.heroku+json; version=3"
+
     def find options={}
-      JSON.parse RestClient.get "#{@base_uri}/#{@endpoint}", accept: @accept_headers, authorization: "Bearer #{@auth_key}"
+      JSON.parse RestClient.get "#{BASE_URL}/#{@endpoint}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
     end
   end
 end
