45ce0ee00733488b36dbb126fe7c2947219b7936
James Liu <jaycliu@gmail.com>
2014-08-24 13:55:31 -0700

    add create method to base class

--- lib/heart_of_heroku/base.rb
+++ lib/heart_of_heroku/base.rb
@@ -8,5 +8,11 @@ module HeartOfHeroku
       path += "/#{option}" unless option.empty?
       JSON.parse RestClient.get "#{path}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
     end
+
+    def create endpoint, option=''
+      path = "#{BASE_URI}/#{endpoint}"
+      path += "/#{option}" unless option.empty?
+      JSON.parse RestClient.post "#{path}", accept: ACCEPT_HEADERS, authorization: "Bearer #{@auth_key}"
+    end
   end
 end
